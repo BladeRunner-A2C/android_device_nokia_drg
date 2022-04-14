dd19c1c997bbaab1ad36ab9327a89e38b7a18b82
Alexander Koskovich <akoskovich@pm.me>
2022-04-13 23:46:09 -0700

    drg: Drop all component overrides
     * Qualcomm no longer disables these for targets
       that support RRO, e.g. all QSSI targets.
    
    Change-Id: I0372817623e8d0e58926fbfb519671c3caa07b86

--- device.mk
+++ device.mk
@@ -101,10 +101,6 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
-# Component overrides
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
