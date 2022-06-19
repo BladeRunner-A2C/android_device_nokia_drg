bbf3403830c3e037ea754b4adf0792e6aa48869c
BladeRunner-A2C <prisoner56898@protonmail.com>
2022-06-19 16:22:55 +0600

    drg: Run `clang-format` treewide
    Signed-off-by: BladeRunner-A2C <prisoner56898@protonmail.com>

--- libshims/gui_shim.cpp
+++ libshims/gui_shim.cpp
@@ -1,12 +1,14 @@
-#include <stdint.h>
 #include <gui/IGraphicBufferProducer.h>
+#include <stdint.h>
 
 namespace android {
-    extern "C" void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEbRKNS1_INS_7IBinderEEE(
-        const sp<IGraphicBufferProducer>& bufferProducer, bool controlledByApp, const sp<IBinder>& surfaceControlHandle);
+extern "C" void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEbRKNS1_INS_7IBinderEEE(
+    const sp<IGraphicBufferProducer> &bufferProducer, bool controlledByApp,
+    const sp<IBinder> &surfaceControlHandle);
 
-    extern "C" void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb(
-        const sp<IGraphicBufferProducer>& bufferProducer, bool controlledByApp) {
-            _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEbRKNS1_INS_7IBinderEEE(bufferProducer, controlledByApp, NULL);
-    }
+extern "C" void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb(
+    const sp<IGraphicBufferProducer> &bufferProducer, bool controlledByApp) {
+    _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEbRKNS1_INS_7IBinderEEE(
+        bufferProducer, controlledByApp, NULL);
 }
+}  // namespace android
